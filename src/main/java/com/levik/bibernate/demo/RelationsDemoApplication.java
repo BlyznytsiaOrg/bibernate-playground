8796d881382e3058760db957d36385860bba1fd2
Andrew Pikozh <pikozh13@gmail.com>
2024-02-17 02:34:36 +0200

    add one to one demo

---
+++ src/main/java/com/levik/bibernate/demo/RelationsDemoApplication.java
@@ -0,0 +1,62 @@
+package com.levik.bibernate.demo;
+
+import com.levik.bibernate.demo.multirelations.Address;
+import com.levik.bibernate.demo.multirelations.House;
+import com.levik.bibernate.demo.multirelations.Profile;
+import com.levik.bibernate.demo.multirelations.User;
+import io.github.blyznytsiaorg.bibernate.Persistent;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+
+@Slf4j
+public class RelationsDemoApplication {
+    public static final String ENTITY_PACKAGE = "com.levik.bibernate.demo.multirelations";
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Persistent persistent = Persistent.withDefaultConfiguration(ENTITY_PACKAGE);
+
+        try (var bibernateEntityManager = persistent.createBibernateEntityManager()) {
+            var bibernateSessionFactory = bibernateEntityManager.getBibernateSessionFactory();
+            try (var session = bibernateSessionFactory.openSession()) {
+
+                var house = new House();
+                house.setName("House name 1");
+
+                House savedHouse = session.save(House.class, house);
+
+                var address = new Address();
+                address.setName("Name 1");
+                address.setHouse(savedHouse);
+
+                session.save(Address.class, address);
+
+                var profile = new Profile();
+                profile.setNickname("Petrushka");
+                session.save(Profile.class, profile);
+
+                var user = new User();
+                user.setFirstName("Petrik");
+                user.setLastName("Pyatochkin");
+                user.setAddress(address);
+                user.setProfile(profile);
+
+                session.save(User.class, user);
+
+                session.flush();
+            }
+
+            try (var bibernateSession = bibernateSessionFactory.openSession()) {
+                Optional<User> optionalUser = bibernateSession.findById(User.class, 1L);
+                optionalUser.ifPresent(user -> {
+                    log.info("User's first name {}", user.getFirstName());
+                    log.info("User's last name {}", user.getLastName());
+                    log.info("User's address {}", user.getAddress());
+                    log.info("User's profile {}", user.getProfile());
+                });
+            }
+        }
+    }
+}
